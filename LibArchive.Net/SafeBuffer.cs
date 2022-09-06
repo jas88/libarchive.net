f7f400d83dd5f665fe0f94228c7ffc524d3ea3c0
James A Sutherland <>
2022-09-05 19:15:21 -0500

    Add buffer and string handling wrappers

--- LibArchive.Net/SafeBuffer.cs
+++ LibArchive.Net/SafeBuffer.cs
@@ -1,6 +1,21 @@
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
 namespace LibArchive.Net;
 
-public class SafeBuffer
+public class SafeBuffer : SafeHandleZeroOrMinusOneIsInvalid
 {
-    
+    public IntPtr Ptr => handle;
+
+    public SafeBuffer(int size) : base(true)
+    {
+        handle = Marshal.AllocHGlobal(size);
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        Marshal.FreeHGlobal(handle);
+        return true;
+    }
 }
--- LibArchive.Net/SafeStringBuffer.cs
+++ LibArchive.Net/SafeStringBuffer.cs
@@ -1,6 +1,23 @@
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
 namespace LibArchive.Net;
 
-public class SafeStringBuffer
+public class SafeStringBuffer : SafeHandleZeroOrMinusOneIsInvalid
 {
+    public IntPtr Ptr => handle;
     
+    public SafeStringBuffer(string s) : base(true)
+    {
+        if (s is null)
+            throw new ArgumentException(nameof(s));
+        handle = Marshal.StringToCoTaskMemUTF8(s);
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        Marshal.FreeCoTaskMem(handle);
+        return true;
+    }
 }
