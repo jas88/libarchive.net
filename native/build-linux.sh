4210c4e07ccc28f9d57a6bfb514ada6e88b4faf9
James A Sutherland <>
2022-09-07 19:24:27 -0500

    Generate .dylib, copy to Linux, build .so and test there

--- native/build-linux.sh
+++ native/build-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
+export NCPU=`nproc`
 export PREFIX=`pwd`/local
 export CPPFLAGS="-I$PREFIX/include -I$PREFIX/x86_64-linux-musl/include -I$PREFIX/lib/gcc/x86_64-linux-musl/9.2.0/include"
 export CFLAGS="-fPIC -O2 $CPPFLAGS -static-libgcc"
@@ -10,6 +11,9 @@ export PATH="$PREFIX/bin:$PREFIX/x86_64-linux-musl/bin:$PATH"
 
 curl -sL https://github.com/richfelker/musl-cross-make/archive/refs/tags/v0.9.9.tar.gz | tar xzf -
 curl -sL https://github.com/libarchive/libarchive/releases/download/v3.6.1/libarchive-3.6.1.tar.xz | tar xJf -
+curl -sL https://github.com/lz4/lz4/archive/refs/tags/v1.9.4.tar.gz | tar xzf -
+curl -sL https://github.com/facebook/zstd/releases/download/v1.5.2/zstd-1.5.2.tar.gz | tar xzf -
+curl -sL http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz | tar xzf -
 curl -sL https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.9.14/libxml2-v2.9.14.tar.bz2 | tar xjf -
 curl -sL https://www.sourceware.org/pub/bzip2/bzip2-latest.tar.gz | tar xzf -
 curl -sL https://zlib.net/zlib-1.2.12.tar.xz | tar xJf -
@@ -22,33 +26,39 @@ COMMON_CONFIG += --disable-nls
 GCC_CONFIG += --disable-libitm
 GCC_CONFIG += --enable-default-pie
 EOC
-make -sj`nproc` install OUTPUT=$PREFIX 2>&1 >musl.log || cat musl.log
+make -sj$NCPU install OUTPUT=$PREFIX 2>&1 >musl.log || cat musl.log
 
 export CC=x86_64-linux-musl-gcc
 export CXX=x86_64-linux-musl-g++
 cd ..
-make -j`nproc` -sC bzip2-1.0.8 install PREFIX=$PREFIX CFLAGS="-fPIC -O2 -D_FILE_OFFSET_BITS=64" CC=$CC
+make -j$NCPU -sC lz4-1.9.4 install
+make -j$NCPU -sC zstd-1.5.2 install
+make -j$NCPU -sC bzip2-1.0.8 install PREFIX=$PREFIX CFLAGS="-fPIC -O2 -D_FILE_OFFSET_BITS=64" CC=$CC
+
+cd lzo-2.10
+./configure --prefix=$PREFIX
+make -sj$NCPU install
 
 cd zlib-1.2.12
 ./configure --static --prefix=$PREFIX
-make -sj`nproc` install
+make -sj$NCPU install
 
 cd ../xz-5.2.5
 ./configure --with-pic --disable-shared --prefix=$PREFIX
-make -sj`nproc` install
+make -sj$NCPU install
 
 cd ../libxml2-v2.9.14
 ./autogen.sh --enable-silent-rules --disable-shared --enable-static --prefix=$PREFIX --without-python --with-zlib=$PREFIX/../zlib-1.2.12 --with-lzma=$PREFIX/../xz-5.2.5
-make -sj`nproc` install
+make -sj$NCPU install
 
 cd ../libarchive-*
 export LIBXML2_PC_CFLAGS=-I$PREFIX/include/libxml2
 export LIBXML2_PC_LIBS=-L$PREFIX
 ./configure --prefix=$PREFIX --disable-bsdtar --disable-bsdcat --disable-bsdcpio --enable-posix-regex-lib=libc --with-pic --with-sysroot --with-lzo2
-make -sj`nproc` install
+make -sj$NCPU install
 
 cd ..
-gcc -shared -o libarchive.so -Wl,--whole-archive local/lib/libarchive.a -Wl,--no-whole-archive local/lib/liblzma.a local/lib/libz.a local/lib/libbz2.a  local/x86_64-linux-musl/lib/libc.a -nostdlib
+gcc -shared -o libarchive.so -Wl,--whole-archive local/lib/libarchive.a -Wl,--no-whole-archive local/lib/libbz2.a local/lib/libz.a local/lib/libxml2.a local/lib/liblzma.a local/lib/liblzo2.a local/lib/libzstd.a local/lib/liblz4.a local/x86_64-linux-musl/lib/libc.a -nostdlib
 
 cat > test.c <<EOT
 #include <stdio.h>
@@ -63,3 +73,5 @@ gcc -o test test.c
 file libarchive.so
 ldd libarchive.so
 
+gcc -o nativetest nativetest.c local/lib/libarchive.a -Llocal/lib -Ilocal/include -llz4 -lzstd -liconv -lbz2
+./nativetest
--- native/build-macos.sh
+++ native/build-macos.sh
@@ -3,16 +3,28 @@
 set -e
 export PREFIX=`pwd`/local
 export NCPU=`sysctl -n hw.ncpu`
+export CPPFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib -liconv"
 export CFLAGS="-fPIC -O2 -D_FILE_OFFSET_BITS=64 -arch arm64 -arch x86_64"
 
 curl -sL https://github.com/libarchive/libarchive/releases/download/v3.6.1/libarchive-3.6.1.tar.xz | tar xJf -
+curl -sL https://github.com/lz4/lz4/archive/refs/tags/v1.9.4.tar.gz | tar xzf -
+curl -sL https://github.com/facebook/zstd/releases/download/v1.5.2/zstd-1.5.2.tar.gz | tar xzf -
+curl -sL http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz | tar xzf -
 curl -sL https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.9.14/libxml2-v2.9.14.tar.bz2 | tar xjf -
 curl -sL https://www.sourceware.org/pub/bzip2/bzip2-latest.tar.gz | tar xzf -
 curl -sL https://zlib.net/zlib-1.2.12.tar.xz | tar xJf -
 curl -sL https://tukaani.org/xz/xz-5.2.5.tar.xz | tar xJf -
 
+make -j$NCPU -sC lz4-1.9.4 install
+make -j$NCPU -sC zstd-1.5.2 install
 make -j$NCPU -sC bzip2-1.0.8 install PREFIX=$PREFIX
-cd zlib-1.2.12
+
+cd lzo-2.10
+./configure --prefix=$PREFIX
+make -sj$NCPU install
+
+cd ../zlib-1.2.12
 ./configure --static --prefix=$PREFIX
 make -sj$NCPU install
 cd ../xz-5.2.5
@@ -25,8 +37,10 @@ make -sj$NCPU install
 cd ../libarchive-*
 export LIBXML2_PC_CFLAGS=-I$PREFIX/include/libxml2
 export LIBXML2_PC_LIBS="-L$PREFIX -lxml2"
-./configure --prefix=$PREFIX --disable-bsdtar --disable-bsdcat --disable-bsdcpio --enable-posix-regex-lib=libc --with-pic --with-sysroot --with-lzo2
+./configure --prefix=$PREFIX --enable-silent-rules --disable-dependency-tracking --enable-static --disable-shared --disable-bsdtar --disable-bsdcat --disable-bsdcpio --disable-rpath --enable-posix-regex-lib=libc --enable-xattr --enable-acl --enable-largefile --with-pic --with-zlib --with-bz2lib --with-libb2 --with-iconv --with-lz4 --with-zstd --with-lzma --with-lzo2 --with-cng
 make -sj$NCPU install
 cd ..
 
-clang -arch arm64 -arch x86_64 -dynamiclib -o libarchive.dylib -Wl,-force_load local/lib/libarchive.a local/lib/libbz2.a local/lib/libz.a local/lib/libxml2.a -liconv
+clang -arch arm64 -arch x86_64 -dynamiclib -shared -o libarchive.dylib -Wl,-force_load local/lib/libarchive.a local/lib/libbz2.a local/lib/libz.a local/lib/libxml2.a local/lib/liblzma.a local/lib/liblzo2.a local/lib/libzstd.a local/lib/liblz4.a -liconv
+gcc -o nativetest nativetest.c local/lib/libarchive.a -Llocal/lib -Ilocal/include -llz4 -lzstd -liconv -lbz2
+./nativetest
