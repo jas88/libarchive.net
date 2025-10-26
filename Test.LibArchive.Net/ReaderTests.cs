c98812bf83f7956e5bb0e3fac4983a4af2c8bb4a
James A Sutherland <j@sutherland.pw>
2025-10-26 16:02:28 -0500

    Fix remaining C# 9+ features for net462 compatibility
    - Replace target-typed new() with explicit ExtractedEntry(...)
    - Replace HashCode.Combine with manual hash code (not available in net462)
    - Use traditional hash combining algorithm (17 * 31 pattern)

--- Test.LibArchive.Net/ReaderTests.cs
+++ Test.LibArchive.Net/ReaderTests.cs
@@ -41,11 +41,11 @@ public class SevenZipTests
 
         Assert.That(extracted, Is.EquivalentTo(new Dictionary<string, ExtractedEntry>
         {
-            { "subdir/", new(EntryType.Directory, emptyHash) },
-            { "empty", new(EntryType.RegularFile, emptyHash) },
-            { "subdir/empty", new(EntryType.RegularFile, emptyHash) },
-            { "1gzero", new(EntryType.RegularFile, "49-BC-20-DF-15-E4-12-A6-44-72-42-1E-13-FE-86-FF-1C-51-65-E1-8B-2A-FC-CF-16-0D-4D-C1-9F-E6-8A-14") },
-            { "1krandom", new(EntryType.RegularFile, "DA-26-F3-BE-7A-9A-2D-F1-0A-49-35-87-8B-18-C8-FF-FE-2B-96-13-EA-CD-E2-C8-67-DF-8A-A2-5D-41-0D-0A") },
+            { "subdir/", new ExtractedEntry(EntryType.Directory, emptyHash) },
+            { "empty", new ExtractedEntry(EntryType.RegularFile, emptyHash) },
+            { "subdir/empty", new ExtractedEntry(EntryType.RegularFile, emptyHash) },
+            { "1gzero", new ExtractedEntry(EntryType.RegularFile, "49-BC-20-DF-15-E4-12-A6-44-72-42-1E-13-FE-86-FF-1C-51-65-E1-8B-2A-FC-CF-16-0D-4D-C1-9F-E6-8A-14") },
+            { "1krandom", new ExtractedEntry(EntryType.RegularFile, "DA-26-F3-BE-7A-9A-2D-F1-0A-49-35-87-8B-18-C8-FF-FE-2B-96-13-EA-CD-E2-C8-67-DF-8A-A2-5D-41-0D-0A") },
         }));
     }
 
@@ -59,11 +59,11 @@ public class SevenZipTests
 
         Assert.That(extracted, Is.EquivalentTo(new Dictionary<string, ExtractedEntry>
         {
-            { "subdir", new(EntryType.Directory, emptyHash) },
-            { "empty", new(EntryType.RegularFile, emptyHash) },
-            { "subdir/empty", new(EntryType.RegularFile, emptyHash) },
-            { "1gzero", new(EntryType.RegularFile, "8B-A9-05-B1-20-A7-C8-D7-89-0F-AB-53-3B-75-65-C9-2C-3D-30-B7-E2-98-41-DF-52-C0-CF-F3-9D-3C-F1-A2") },
-            { "1krandom", new(EntryType.RegularFile, "DA-26-F3-BE-7A-9A-2D-F1-0A-49-35-87-8B-18-C8-FF-FE-2B-96-13-EA-CD-E2-C8-67-DF-8A-A2-5D-41-0D-0A") },
+            { "subdir", new ExtractedEntry(EntryType.Directory, emptyHash) },
+            { "empty", new ExtractedEntry(EntryType.RegularFile, emptyHash) },
+            { "subdir/empty", new ExtractedEntry(EntryType.RegularFile, emptyHash) },
+            { "1gzero", new ExtractedEntry(EntryType.RegularFile, "8B-A9-05-B1-20-A7-C8-D7-89-0F-AB-53-3B-75-65-C9-2C-3D-30-B7-E2-98-41-DF-52-C0-CF-F3-9D-3C-F1-A2") },
+            { "1krandom", new ExtractedEntry(EntryType.RegularFile, "DA-26-F3-BE-7A-9A-2D-F1-0A-49-35-87-8B-18-C8-FF-FE-2B-96-13-EA-CD-E2-C8-67-DF-8A-A2-5D-41-0D-0A") },
         }));
     }
 
@@ -85,14 +85,23 @@ public class SevenZipTests
         public override bool Equals(object? obj) =>
             obj is ExtractedEntry other && Type == other.Type && ContentHash == other.ContentHash;
 
-        public override int GetHashCode() => HashCode.Combine(Type, ContentHash);
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 31 + Type.GetHashCode();
+                hash = hash * 31 + (ContentHash?.GetHashCode() ?? 0);
+                return hash;
+            }
+        }
     }
 #else
     private record ExtractedEntry(EntryType Type, string ContentHash);
 #endif
 
     private ExtractedEntry ToExtractedEntry(Entry entry) =>
-        new(entry.Type, ContentHash(entry));
+        new ExtractedEntry(entry.Type, ContentHash(entry));
 
     private string ContentHash(Entry entry)
     {
